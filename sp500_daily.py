31e633f7ee1eff68fabd3ffa773df2389ad93e3d
James Maslek <jmaslek11@gmail.com>
2023-07-27 17:20:12 -0400

    Update sp500_daily.py

--- sp500_daily.py
+++ sp500_daily.py
@@ -8,8 +8,8 @@ sp_ticks = df["Symbol"].to_list()
 sp_ticks_forYF = [tick.replace(".","-") for tick in sp_ticks]
 
 closes = pd.DataFrame()
-for idx in range(0,len(sp_ticks_forYF),20):
-    df = yf.download(sp_ticks_forYF[idx:idx+20],period="1y", progress=False)["Adj Close"]
+for idx in range(0,len(sp_ticks_forYF),10):
+    df = yf.download(sp_ticks_forYF[idx:idx+10],period="1y", progress=False)["Adj Close"]
     closes = pd.concat([df, closes], axis=1)
     time.sleep(2)
 closes.to_csv("SP500_prices_1yr.csv")
